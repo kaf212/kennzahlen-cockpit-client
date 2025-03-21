c8ce4c307b18705265d1534a9a2f96d20319f0ff
kaf212 <kaf221122@gmail.com>
2025-03-21 17:21:51 +0100

    translate german account names according to ERD

--- customFigureBuilder.js
+++ customFigureBuilder.js
@@ -1,8 +1,36 @@
 const translations = {
-    "Anlagevermögen": "fixed_assets",
+    "Aktiven": "actives",
     "Umlaufvermögen": "current_assets",
+    "Flüssige Mittel": "liquid_assets",
+    "Kasse": "cash",
+    "Post": "postal",
     "Bank": "bank",
-    "Verb. L+L": "liabilities"
+    "Ford. L+L": "receivables",
+    "Warenvorrat": "stocks",
+    "Anlagevermögen": "fixed_assets",
+    "Maschinen": "machines",
+    "Mobilien": "movables",
+    "Immobilien": "real_estate",
+    "Passiven": "passives",
+    "Fremdkapital": "debt",
+    "Kurzfristiges FK": "short_term",
+    "Verb. L+L": "liabilities",
+    "Langfristiges FK": "long_term",
+    "Passivdarlehen": "loans",
+    "Hypothek": "mortgage",
+    "Eigenkapital": "equity",
+    "Aktienkapital": "shares",
+    "Ges. Gewinnreserve": "legal_reserve",
+    "Gewinnvortrag": "retained_earnings",
+    "Betriebsaufwand": "operating_expense",
+    "Personalaufwand": "staff_expense",
+    "Sonst. Betriebsaufwand": "other_expenses",
+    "Abschreibungen": "depreciation",
+    "Finanzaufwand": "financial_expense",
+    "Liegenschaftsaufwand": "real_estate_expense",
+    "Betriebsertrag": "operating_income",
+    "Finanzertrag": "financial_income",
+    "Liegenschaftsertrag": "real_estate_income"
 }
 
 Array.from(document.getElementsByClassName("accountButton")).forEach(button => {
