f976e12096e92b0208d59d5d95701f07dc3604fa
kaf212 <kaf221122@gmail.com>
2025-03-30 12:50:14 +0200

    commented all functions in customFigureBuilder.js

--- customFigureBuilder.js
+++ customFigureBuilder.js
@@ -40,10 +40,20 @@ const translations = {
 }
 
 function addTabButtonEventListeners() {
+    /*
+    Adds the necessary EventListeners to the buttons that switch between
+    the tabs containing the account buttons (actives, passives etc.)
+    The EventListeners get triggered with a click and then iterate over all the four tabs and hides them
+    except the one that contains the accounts of the given account group. For example: a press on the "Aktiven" button
+    will hide all tabs except the actives tab. The link between the buttons and the tabs is created with the
+    data-account-group attribute in the HTML-tab-elements.
+    :param: none
+    :return: void
+     */
     Array.from(document.getElementsByClassName("tab-button")).forEach(tabButton=>{
         tabButton.addEventListener( "click",(event) =>{
             /*
-            When a tab button is clicked, it iterates over all tab buttons and adds the invisible class
+            When a tab button is clicked, it iterates over all tabs and adds the invisible class
             except for the account group that corresponds to the given button
              */
             Array.from(document.getElementsByClassName("custom-figure-builder-tab")).forEach(tab=>{
@@ -62,6 +72,12 @@ function addTabButtonEventListeners() {
 
 
 function createAccountButtons() {
+    /*
+    Iterates over all account groups and iterates again over all the nested accounts in said account group and adds
+    a HTML element of a button for this account to the respective account tab in the custom figure builder.
+    :param: none
+    :return: void
+     */
     for (const [accountGroup, accounts] of Object.entries(translations)) {
         for (const [germanAccount, englishAccount] of Object.entries(accounts)) {
             const targetAccountButtonDiv = `${accountGroup}Tab` // Is equal to the id of the account button divs
@@ -108,6 +124,14 @@ function addButtonEventListeners() {
 }
 
 function parseFormulaString(formulaStr) {
+    /*
+    Iterates over all english account names in the translations object and checks, if they are a substring of the
+    provided formula string. If yes, then the substring is replaced with the corresponding german translation from the
+    translations object.
+    At the end, all whitespaces are removed from the formula to make it processable in the backend.
+    :param: formulaStr (str): The german formula provided by the UI in the formula field
+    :return: formulaStr (str): The translated formula without whitespaces
+     */
     for (const [accountGroup, accounts] of Object.entries(translations)) {
         for (const [germanAccount, englishAccount] of Object.entries(accounts)) {
             if (formulaStr.includes(germanAccount)) {
@@ -125,17 +149,31 @@ function parseFormulaString(formulaStr) {
 }
 
 function addSubmitEventListener() {
+    /*
+    Adds the EventListener to the custom figure builder form that gets triggered with the form submit.
+    Upon submitting, the formula string is read from the formula field in the UI and passed to parseFormulaString()
+    The parsed result is then passed to saveNewCustomKeyFigure() to be sent to the server.
+    :param: none
+    :return: void
+     */
     const formulaField = document.getElementById("formulaField")
     const customFigureBuilderForm = document.getElementById("customFigureBuilderForm")
     customFigureBuilderForm.addEventListener("submit", (event)=>{
         const parsedFormula = parseFormulaString(formulaField.value)
         const formulaName = document.getElementById("formulaNameField").value
         saveNewCustomKeyFigure(formulaName, parsedFormula)
-        event.preventDefault()
+        event.preventDefault() // Prevent page from refreshing
     })
 }
 
 function handleServerResponse(res) {
+    /*
+    Gets called upon receiving a response from the server in saveNewCustomKeyFigure() to process the response.
+    The response message will be displayed in the infobox in the UI and the font color will be green, if the status is
+    20x and red if it's 40x.
+    :param: res (Response): The HTTP-response from the backend
+    :return: void
+     */
     const statusCode = res.status.toString()
 
     res.json().then(data=>{
@@ -156,6 +194,13 @@ function handleServerResponse(res) {
 }
 
 function saveNewCustomKeyFigure(formulaName, formulaStr) {
+    /*
+    Sends a POST request to the backend to save the new custom key figure on the server.
+    The response is then passed to handleServerResponse() to display the success or error message in the UI.
+    :param: formulaName (str): The name of the new custom key figure
+    :param: formulaStr (str): The translated formula
+    :return: void
+     */
     // Causes NetworkError in firefox for some reason. (https://github.com/kaf212/kennzahlen-cockpit-client/issues/7)
     fetch("http://localhost:5000/customKeyFigures", {
         method: "POST",
