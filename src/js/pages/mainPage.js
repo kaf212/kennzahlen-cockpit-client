36f9e2c64affa2e485b26227cd8abb3889f85a92
laiba-bzz <laiba@bzz.ch>
2025-04-06 11:34:22 +0200

    move EventListeners for main page to mainPage.js

--- src/js/pages/mainPage.js
+++ src/js/pages/mainPage.js
@@ -6,3 +6,34 @@ function logout() {
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("logoutButton").addEventListener("click", logout);
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Tabs wechseln
+    function showTab(tab) {
+        document.getElementById("graph-section").classList.add("hidden");
+        document.getElementById("table-section").classList.add("hidden");
+
+        document.getElementById(tab + "-section").classList.remove("hidden");
+
+        document.querySelectorAll(".tab-button").forEach(button => {
+            button.classList.remove("border-gray-800");
+            button.classList.add("border-transparent");
+        });
+
+        document.getElementById("tab-" + tab).classList.add("border-gray-800");
+        document.getElementById("tab-" + tab).classList.remove("border-transparent");
+    }
+
+    if (document.getElementById("tab-graph") && document.getElementById("tab-table")) {
+        showTab('graph');
+
+        document.getElementById("tab-graph").addEventListener("click", function() {
+            showTab('graph');
+        });
+
+        document.getElementById("tab-table").addEventListener("click", function() {
+            showTab('table');
+        });
+    }
+
+});
--- src/script.js
+++ src/script.js
@@ -1,35 +1,5 @@
 import {sendServerRequest, addInfoBoxEventListener} from "./js/utils/serverResponseHandling.js";
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Tabs wechseln
-    function showTab(tab) {
-        document.getElementById("graph-section").classList.add("hidden");
-        document.getElementById("table-section").classList.add("hidden");
-
-        document.getElementById(tab + "-section").classList.remove("hidden");
-
-        document.querySelectorAll(".tab-button").forEach(button => {
-            button.classList.remove("border-gray-800");
-            button.classList.add("border-transparent");
-        });
-
-        document.getElementById("tab-" + tab).classList.add("border-gray-800");
-        document.getElementById("tab-" + tab).classList.remove("border-transparent");
-    }
-
-    if (document.getElementById("tab-graph") && document.getElementById("tab-table")) {
-        showTab('graph');
-
-        document.getElementById("tab-graph").addEventListener("click", function() {
-            showTab('graph');
-        });
-
-        document.getElementById("tab-table").addEventListener("click", function() {
-            showTab('table');
-        });
-    }
-
-});
 document.addEventListener("DOMContentLoaded", () => {
     const companyInput = document.getElementById("companyNameInput");
     const companyList = document.getElementById("companyList");
