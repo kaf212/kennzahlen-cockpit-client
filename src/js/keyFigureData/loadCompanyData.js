65a6d6c435d63c87721a5fc8fc1025a8538f2695
kaf212 <kaf221122@gmail.com>
2025-05-02 13:13:50 +0200

    remove unused function

--- src/js/keyFigureData/loadCompanyData.js
+++ src/js/keyFigureData/loadCompanyData.js
@@ -8,16 +8,3 @@ export function getCurrentKeyFigureData() {
     const keyFigureData = sendServerRequest("GET", `http://localhost:5000/api/keyFigures/current/${companyId}`, null, false);
     return keyFigureData;
 }
-export async function loadHistoricKeyFigures(companyId) {
-    try {
-        const response = await fetch(`http://localhost:5000/api/keyFigures/historic/${companyId}`);
-        if (!response.ok) {
-            throw new Error("Fehler beim Laden historischer Daten");
-        }
-        return await response.json();
-    } catch (error) {
-        console.error("Fehler beim Laden der historischen Kennzahlen:", error);
-        return null;
-    }
-}
-
